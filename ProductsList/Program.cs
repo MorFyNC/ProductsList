e7897fec447784752303fa71989dabf814d53feb
MorFy <bulat.hisambeev@gmail.com>
2023-10-19 10:48:10 +0300

    Добавьте файлы проекта.

---
+++ ProductsList/Program.cs
@@ -0,0 +1,56 @@
+﻿using Class;
+using Newtonsoft.Json;
+
+Product milk = ProductFabric.Create("80 рублей", "1 литр", "Простоквашино", 1000, true);
+Product bread = ProductFabric.Create("40 рублей", "1 буханка", "БКК", 300, false);
+Product crisps = ProductFabric.Create("90 рублей", "1 пачка", "Lays", 100, false);
+Product soap = ProductFabric.Create("20 рублей", "1 кусок", "Avon", 150, false);
+Product sausage = ProductFabric.Create("250 рублей", "20 штук/1 связка", "Папа может", 1500, false);
+Product water = ProductFabric.Create("35 рублей", "1 литр", "Святой источник", 1000, true);
+Product spoon = ProductFabric.Create("200 рублей", "5 штук/1 набор", "Амет", 200, false);
+Product notebook = ProductFabric.Create("40 рублей", "1 шт/96 листов", "Каждый день", 50, false);
+Product pen = ProductFabric.Create("10 рублей", "1 шт", "BIC", 50, false);
+Product hat = ProductFabric.Create("2000 рублей", "1 шт", "GUCCI", 250, false);
+
+List<Product> products = new List<Product>() { milk, bread, crisps, soap, sausage, water, spoon, notebook, pen, hat };
+Console.WriteLine("\tОбычный список");
+Print(jsonConvert(products));
+Console.WriteLine();
+Console.WriteLine("\tОтсортированный по весу список:");
+SortByWeigth(products);
+
+List<string> jsonConvert(List<Product> products)
+{
+    List<string> list = new List<string>();
+    foreach (Product product in products)
+    {
+        string json = JsonConvert.SerializeObject(product);
+        list.Add(json);
+    }
+    return list;
+}
+
+void Print(List<string> list)
+{
+    foreach (string item in list)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+void SortByWeigth(List<Product> products)
+{
+    for (int j = 0; j < products.Count; j++)
+    {
+        for (int i = 0; i < products.Count; i++)
+        {
+            if (i < 9 && products[i].weight < products[i + 1].weight)
+            {
+                var temp = products[i];
+                products[i] = products[i + 1];
+                products[i + 1] = temp;
+            }
+        }
+    }
+    Print(jsonConvert(products));
+}
---
+++ ProductsList/class.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace Class
+{
+    class Product
+    {
+        public string cost;
+        public string size;
+        public string name;
+        public int weight;
+        public bool fragility;
+
+        public Product(string cost, string size, string name, int weight, bool fragility)
+        {
+            this.cost = cost;
+            this.size = size;
+            this.name = name;
+            this.weight = weight;
+            this.fragility = fragility;
+        }
+    }
+
+    static class ProductFabric
+    {
+        public static Product Create(string cost, string size, string name, int weight, bool fragility)
+        {
+            return new Product(cost, size, name, weight, fragility);
+        }
+
+    }
+}
